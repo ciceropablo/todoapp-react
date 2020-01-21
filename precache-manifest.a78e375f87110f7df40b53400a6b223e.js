222033864ccfc8027131c1c2e93e8709a5601026
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-21 20:43:54 +0000

    Deploy ciceropablo/todoapp-react to github.com/ciceropablo/todoapp-react.git:gh-pages

---
+++ precache-manifest.a78e375f87110f7df40b53400a6b223e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63a5909e463a26dcf57822caf954f759",
+    "url": "/todoapp-react/index.html"
+  },
+  {
+    "revision": "630068533c36fc6ed989",
+    "url": "/todoapp-react/static/css/main.010eb2a3.chunk.css"
+  },
+  {
+    "revision": "44dc607d5fc0305e1fe8",
+    "url": "/todoapp-react/static/js/2.c35274d0.chunk.js"
+  },
+  {
+    "revision": "630068533c36fc6ed989",
+    "url": "/todoapp-react/static/js/main.db76e76e.chunk.js"
+  },
+  {
+    "revision": "6adfecde951fce004cb9",
+    "url": "/todoapp-react/static/js/runtime-main.4bb1bdab.js"
+  }
+]);
